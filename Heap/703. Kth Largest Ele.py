2577ad3f41eacb7b4c36fdbcb997d5f91b84fe5e
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2022-06-02 15:30:14 +0800

    Create 703. Kth Largest Ele.py

---
+++ Heap/703. Kth Largest Ele.py
@@ -0,0 +1,16 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = nums
+        self.k = k
+        heapq.heapify(self.heap)
+        while len(self.heap) > k:
+            heapq.heappop(self.heap)
+            
+
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        return self.heap[0]
