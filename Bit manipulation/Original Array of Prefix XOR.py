0c301c5ee4e834fa72b6675f5d7b810995526ab9
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2022-10-09 12:57:47 +0800

    Original Array of Prefix XOR

---
+++ Bit manipulation/Original Array of Prefix XOR.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        res = [0] * len(pref)
+        
+        res[0] = pref[0]
+        
+        
+        for i in range(1, len(pref)):
+            res[i] = pref[i] ^ pref[i - 1]
+            
+            
+        return res
