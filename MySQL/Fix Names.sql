73fba1f035611b2da5864e62d7a2b069bb45ec6a
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2022-10-11 14:13:31 +0800

    sql

---
+++ MySQL/Fix Names.sql
@@ -0,0 +1,4 @@
+
+SELECT user_id, CONCAT(UPPER(SUBSTRING(name, 1, 1)), LOWER(SUBSTRING(name, 2))) AS name
+FROM Users
+ORDER BY user_id;
---
+++ MySQL/SELECT PATIENTS.sql
@@ -0,0 +1,4 @@
+
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions LIKE "DIAB1%" OR conditions like "% DIAB1%"
