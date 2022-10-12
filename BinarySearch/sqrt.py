aa4eb351d11ff1b00b7898d22c099c5f43d4926b
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2022-10-12 12:51:57 +0800

    A better understanding of Binary Search

--- BinarySearch/sqrt.py
+++ BinarySearch/sqrt.py
@@ -1,25 +1,31 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        # we can do binary Search here 
-        if x == 0:
-            return 0
+        if x == 0 :
+            return 0 
         if x == 1:
             return 1
-        left = 0 
-        right = x
-        # we are finding the maximal k such that k**2 >= x 
         
-        # 0 ** 2 = 0 
-        # left = 1 
+        
+        # basically I want to find the largest num such that num^2 <= x 
+        
+        res = 0 
+        left = 0 
+        right = x 
+        
+        # so what I want to do is that to return the final answer NOT in the while loop 
+        
+        
+        # hence, everytime I find a valid answer, I will assign res to that possible answer 
+        # then, move on to find the next best answer, left + 1 BECAUSE the current mid is already the answer 
+        
         while left < right:
             mid = left + (right - left)//2 
-            
             if mid**2 > x:
-                right = mid  
-            elif mid**2 < x:
-                left = mid + 1
-                
+                right = mid
             else:
-                return mid 
+                res = mid
+                left = mid + 1 
+                
             
-        return left - 1
+            
+        return res
