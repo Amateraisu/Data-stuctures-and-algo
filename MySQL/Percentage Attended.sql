8c0d97a547d67e45127d8163f0d981519399f6be
Lin Kai <87221402+KaiKaizxc@users.noreply.github.com>
2022-10-29 18:55:51 +0800

    Create Percentage Attended.sql

---
+++ MySQL/Percentage Attended.sql
@@ -0,0 +1,5 @@
+SELECT contest_id, ROUND((COUNT(user_id) / (SELECT COUNT(user_id)
+    FROM Users)) * 100, 2) AS percentage
+FROM Register
+GROUP BY contest_id
+ORDER BY percentage DESC, contest_id ASC
